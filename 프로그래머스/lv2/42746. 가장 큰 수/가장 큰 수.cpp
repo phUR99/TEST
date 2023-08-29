50fae2726ee4b2d9c90d71499b6adf053d7445e2
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-08-29 22:37:08 +0900

    Update 가장 큰 수.cpp

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 #include <iostream>
 //true 조건에 등호를 넣었을 때 나올 수 있는 문제
-// compare 함수에서 계속 core dumped 에러가 났는데, 컴파일러 버전에 따라 비교로 들어온 두 인자가 같은 경우 true 를 반환하면 오류가 난다고 한다.
+//compare 함수에서 계속 core dumped 에러가 났는데, 컴파일러 버전에 따라 비교로 들어온 두 인자가 같은 경우 true 를 반환하면 오류가 난다고 한다.
 //https://stackoverflow.com/questions/1541817/sort-function-c-segmentation-fault
 //케이스 11번이 계속 실패
-
+//return에 "0"이 아닌 0을 주어서 나온 실패로 보인다.
 using namespace std;
 bool cmp(const string &a, const string &b)
 {   
