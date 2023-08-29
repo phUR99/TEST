22a85f486b9f84d9fc76fca75434b0d41f0465c1
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-08-29 22:35:22 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+//true 조건에 등호를 넣었을 때 나올 수 있는 문제
+// compare 함수에서 계속 core dumped 에러가 났는데, 컴파일러 버전에 따라 비교로 들어온 두 인자가 같은 경우 true 를 반환하면 오류가 난다고 한다.
+//https://stackoverflow.com/questions/1541817/sort-function-c-segmentation-fault
+//케이스 11번이 계속 실패
+
+using namespace std;
+bool cmp(const string &a, const string &b)
+{   
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = ""; 
+    vector<string> temp;
+    for (auto i : numbers){
+        temp.push_back(to_string(i));
+    }
+    sort(temp.begin(), temp.end(), cmp);
+    if (temp.at(0) == "0") return "0";
+    for(auto i : temp){
+        answer += i;
+    }
+    return answer;
+}
