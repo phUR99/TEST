f6d8eb978dc54350d887d0551a28f124a010277a
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-08-30 16:18:32 +0900

    Update 좌표 정렬하기.cc

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+//벡터의 정렬 짜기 오름차순일 경우 부등호 방향을 <로 가게 설계하기
 bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
 	if (a.first == b.second) if (a.second < b.second) return true;
 	return a < b;
