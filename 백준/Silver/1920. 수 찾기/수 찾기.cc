a5eb700d36685e9822baa561e37e9a5697f5a32b
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-08-31 16:32:57 +0900

    Update 수 찾기.cc

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,3 +1,6 @@
+//이분탐색을 써야하는 이유
+//최악의 경우 10^5번의 탐색을 진행해야 하는데, 순차 탐색일 경우에는 o(N^2)이므로 10억번의 연산이 넘게 필요할 수도 있다.
+//무조건 시간 초과가 뜰 것이므로 algortihm 헤더의 binary_search 를 이용해서 i가 있는 지를 탐색한다.
 #include <iostream>
 #include <algorithm>
 #include <vector>
