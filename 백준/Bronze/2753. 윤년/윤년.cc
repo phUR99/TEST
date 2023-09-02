7c2ea4e30eaecf5d87e6a473b831284a7f60da28
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-02 21:09:00 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int year;
+	cin >> year;
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 ==0)) cout << 1;
+	else cout << 0;
+	
+}
