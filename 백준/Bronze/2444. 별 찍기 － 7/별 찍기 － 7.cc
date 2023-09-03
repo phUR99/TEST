0d44ec44aa0b5e671cac7663067871a4f37e401d
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-03 16:18:26 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 1; j <= n - i; j++) cout << ' ';
+		for (int j = 1; j <= 2 * i - 1; j++) cout << '*';
+		cout << "\n";
+
+	}
+	for (int i = n; i >= 1; i--)
+	{
+		for (int j = 1; j <= n - i; j++) cout << ' ';
+		for (int j = 1; j <= 2 * i - 1; j++) cout << '*';
+		cout << "\n";
+
+	}
+}
