c983152c9ff06ba75b9750dfedee19088f8137aa
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-04 23:57:10 +0900

    Update 키로거.cc

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -4,13 +4,15 @@
 #include <algorithm>
 #include <list>
 using namespace std;
-
+//연결 리스트를 이용하는 문제
 void keyloger(string a) {
 	list<char> l;
+	// 비어있어도 주소는 있으니까
 	auto cursor = l.begin();
 	for (auto i : a) {
 		if (l.empty()) {
 			if (isalnum(i)) {
+				//insert!!
 				l.insert(cursor, i);			
 			}
 		}
