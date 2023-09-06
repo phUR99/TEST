7fe2b89f9ada9fc70ace968c953d01be57b19f9b
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-06 10:56:36 +0900

    Update 탑.cc

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -5,7 +5,10 @@
 #include <stack>
 #include <vector>
 using namespace std;
-
+//1. 시간초과 체크,  n은 500000 까지 들어갈 수 있고, O(N^2)의 복잡도로 코드를 짜면 무조건 시간초과난다
+//2. 문제해결방법 -> 거리를 받아줄 좌표와 높이를 받아줄 좌표으 정보를 쌍으로 묶어서 전달하기.
+// 초기값을 int_max로 전달해서 stack이 빌 수 없게 만들어준다. 그 이후로는 stack에서 더 큰 원소를 발견할 때 까지 stack을 pop하면서 체크
+// 이후 발견한다면 체크 후 거리를 증가시켜준다.(최대 N)
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
