369a74fceef23d5adc2ec88f1cc002bdd74cdadb
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-10 14:36:12 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.precision(10);
+	double a, b;
+	cin >> a >> b;
+	cout << a / b;
+}
