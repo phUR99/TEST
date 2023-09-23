d417a4c2135a4cddf3f632da8aa6add5c5015692
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-23 20:41:46 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string temp;
+	int n;
+	cin >> temp >> n;
+	cout << temp[n - 1];
+}
