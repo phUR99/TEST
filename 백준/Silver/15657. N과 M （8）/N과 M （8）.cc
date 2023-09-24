f813b07538aba950b45466bf893ce07fd3af51d0
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-24 19:53:45 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+using namespace std;
+vector<int> a;
+int num[30];
+int n, m;
+int cnt = 0;
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+		{
+			cout << a[num[i]] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	int st = 0;
+	if (k != 0) st = num[k - 1];
+	for (int i = st; i < n; i++)
+	{
+		num[k] = i;
+		func(k + 1);
+	}
+
+
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a.push_back(temp);
+	}
+	sort(a.begin(), a.end());
+	func(0);
+}
+
