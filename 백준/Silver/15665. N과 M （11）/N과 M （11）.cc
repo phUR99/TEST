8b3b3ef25400c6e92a2272f8ea870f63cc21d39a
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-26 15:29:39 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+using namespace std;
+
+int arr[10];
+int num[10];
+int isused[10];
+int n, m;
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+
+	int tmp = 0;
+	for (int i = 0; i < n; i++) {
+		if(tmp != num[i])
+		{
+			arr[k] = num[i];
+			tmp = arr[k];
+			func(k + 1);
+		}
+	}
+}
+
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num[i];
+	}
+	sort(num, num+n);
+	func(0);
+
+}
+
