bb3a1b005414f5a563bf3603848779f2f9209bfd
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-29 22:58:38 +0900

    [Unrated] Title: 타노스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/15802. 타노스/타노스.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+using namespace std;
+
+
+
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << 1;
+}
+
