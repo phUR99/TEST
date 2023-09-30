85672a30790fa39f604a5fcc0cc4ca9dac8eab66
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-09-30 20:41:22 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 272 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+using namespace std;
+int arr[1'000'001];
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + n,greater<>());
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << '\n';
+	}
+}
