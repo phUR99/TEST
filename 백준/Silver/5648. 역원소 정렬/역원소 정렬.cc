ffe789fccd4a7ab783246730b6ea1ee97b9ec703
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-10-01 16:22:53 +0900

    [Silver V] Title: 역원소 정렬, Time: 0 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+using namespace std;
+
+long long arr[1'000'000];
+
+
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+		cin >> temp;
+		reverse(temp.begin(), temp.end());
+		arr[i] = stoll(temp);
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << '\n';
+	}
+}
