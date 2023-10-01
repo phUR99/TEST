228734d8f9d1a078c23604946ee1da9beb94d834
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-10-01 21:41:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+using namespace std;
+
+int ans[1001];
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n; cin >> n;
+	ans[1] = 1; ans[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		ans[i] = (ans[i - 1] + ans[i - 2]) % 10007;
+	}
+	cout << ans[n];
+
+}
