c6573b2f827b4eb6c8eac731f065d9dcd8278274
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-10-05 19:45:18 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+#include <numeric>
+using namespace std;
+#define ll long long
+
+
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n; cin >> n;
+	string ans = (n % 2 == 1) ? "SK" : "CY";
+	cout << ans;
+
+
+
+	
+	
+}
