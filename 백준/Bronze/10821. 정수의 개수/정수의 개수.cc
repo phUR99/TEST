5db6b0440d4e718fa693219d1f3e390926d8d3f0
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-10-07 14:26:28 +0900

    [Bronze II] Title: 정수의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+#include <numeric>
+using namespace std;
+#define ll long long
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string str;
+	cin >> str;
+	int ans = 0;
+	for (auto i : str)
+	{
+		if (i == ',') ans++;
+	}
+	cout << ans + 1;
+}
