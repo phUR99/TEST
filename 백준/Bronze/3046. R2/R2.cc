74392023da568bba87b59817dfb097504de83907
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-10-12 23:19:17 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+#include <numeric>
+
+using namespace std;
+#define ll long long
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int r, s;
+	cin >> r >> s;
+	cout << 2 * s - r;
+
+}
