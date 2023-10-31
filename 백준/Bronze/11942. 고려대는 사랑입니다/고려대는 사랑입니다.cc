e2e4bd8dd5dc16086e4e954caf70696d4fc77760
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-10-31 15:20:24 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <tuple>
+#include <numeric>
+
+using namespace std;
+#define ll long long
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << "고려대학교";
+}
