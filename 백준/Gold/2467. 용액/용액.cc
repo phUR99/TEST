7602421ababe6dc7132cd90b479dcf0addd47213
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-04 14:41:49 +0900

    Update 용액.cc

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -18,10 +18,12 @@ int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
+    //순차탐색으로 진행했을 때 기댓값이 n^2이므로 n의 제한이 100000에서는 불가능하다. nlogn ~= 50000정도로 이분탐색을 이용해서 해결하는게 목표.
     for (int i = 0; i < n; i++) cin >> a[i];
     int ans1 = 1e9 + 1;
     int ans2 = 1e9 + 1;
     for (int i = 0; i < n; i++) { 
+        //lower bound의 특성을 활용해서 항상 더 작은 idx를 반환하는 것을 활용한다.
         int idx = lower_bound(a, a + n, -a[i]) - a; 
         
         if (idx + 1 < n && i != idx + 1 && abs(a[i] + a[idx + 1]) < abs(ans1 + ans2)) {
