559ec310260716035b75ae43c1d1465dfc2acc40
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-04 15:54:01 +0900

    Update 합이 0.cc

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -25,12 +25,14 @@ int main(void) {
     }
     ll ans = 0;
     sort(group.begin(), group.end());
+    // 크기 순은 항상 i, j, k(탐색의 idx)로 하기 위해서 n-2, n-1 순으로 끊음.
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = i + 1; j < n; j++) {
             int goal = group[i] + group[j];
             if (binary_search(group.begin(), group.end(), -goal))
             {
+                //항상 탐색의 결과의 idx는 i, j보다는 크게 하고 싶다 -> 탐색의 반경을 제한하기
                 int idx_l = lower_bound(group.begin()+j+1, group.end(), -goal) - group.begin();
                 int idx_u = upper_bound(group.begin()+j+1, group.end(), -goal) - group.begin();                             
  
