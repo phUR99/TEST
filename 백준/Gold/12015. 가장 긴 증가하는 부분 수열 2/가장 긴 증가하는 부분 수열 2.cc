3f602b649d673d461a7272f8edfebf53947de0e1
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-07 21:27:17 +0900

    Update 가장 긴 증가하는 부분 수열 2.cc

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -25,19 +25,25 @@ int main() {
     {
         cin >> x[i];
     }
+    //가장 긴 수열을 만들어줄 d 배열
     d[0] = x[0];
     int last = 0;
     for (int i = 1; i < n; i++)
     {
+        // 크면
         if (x[i] > d[last])
         {
+            //인덱스를 증가시켜서 올려준다
             d[++last] = x[i];
         }
+        // 작으면
         else
         {
+            // 더 작은 인덱스로 넣어준다.
             int idx = lower_bound(d, d + last, x[i]) - d;
             d[idx] = x[i];
         }
     }
-    cout << last + 1;
+    // 항상 덮어 씌워지기 때문에 다시 작아지더라도 lower_bound를 통해서 idx가 줄어든다.
+    cout << last + 1; (인덱스 + 1)이 길이
 }
