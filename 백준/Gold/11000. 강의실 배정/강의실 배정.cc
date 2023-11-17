11ef1bf08fde0bc0ce4a20791d06266390387ee0
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-17 20:57:24 +0900

    Update 강의실 배정.cc

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -7,6 +7,9 @@ int arr[50];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    //시작시간 기준으로 정렬한 뒤에, 끝나는 시간이 시작시간보다 크다면 우선순위 큐에 계속해서 넣어줌.
+    //만약에 끝나는 시간이 시작시간보다 작거나 같다면 큐의 top을 제거한다.
+    //이렇게 함으로써 끝나는 시간 내의 모든 경우의 수를 체크할 수 있음.
     priority_queue<int, vector<int>, greater<>> end;
     vector<pair<int, int>> sc;
     int n; cin >> n;
