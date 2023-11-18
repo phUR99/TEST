3665528ff07ba1f0cae08c3f9310e6b08f9eeae7
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-18 16:18:54 +0900

    Update 가장 큰 증가하는 부분 수열.cc

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -14,11 +14,13 @@ int main() {
 		cin >> arr[i];
 	}
 	int ans = 0;
+	//d[i] : i번째의 증가하는 부분수열의 최댓값
 	for (int i = 0; i < n; i++)
 	{
 		d[i] = arr[i];
 		for (int j = 0;  j < i; j++)
 		{
+			//arr[i] >arr[j] 라면 i보다 작은 j를 순회하면서 최댓값을 d[i]에 저장한다.
 			if (arr[i] > arr[j]){
 				d[i] = max(d[i], d[j] + arr[i]);				
 			}
