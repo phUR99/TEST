e0ad2275fd4a105e4007a7c75df326ace4f7ad41
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-18 23:25:22 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+int d[1005];
+int a[1005];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        d[i] = 1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++) {
+            if (a[i] < a[j]) {
+                d[i] = max(d[i], d[j] + 1);
+            }
+        }
+    }
+    cout << *max_element(d, d + n);
+}
