599eafb6aa2ad898f606defc8418d42413b2d2e6
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-25 23:15:20 +0900

    Update 가장 긴 증가하는 부분 수열 5.cc

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
-
+//idea: h 배열을 만들어서 최대 높이의 함수를 만들어낸다
+//그러나 이렇게 만들어진 h 배열은 최대 높이만 알 수 있을 뿐, 배열의 성분은 정확하지 않기 때문에(순서가 반영되지 않음) index를 받아 줄 배열이 필요하다
+//idx의 역순으로 a를 탐색하면서 idx랑 일치하는 값을 찾아 배열에 넣은 후 reverse 를 이용하여 출력한다.
 using namespace std;
 #define ll long long
 
