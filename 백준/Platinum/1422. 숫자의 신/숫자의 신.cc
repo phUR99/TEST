49914d978853347057b604febc9fed318eeb1a8f
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-25 19:39:54 +0900

    Update 숫자의 신.cc

--- 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
@@ -3,7 +3,7 @@
 using namespace std;
 #define ll long long
 ll arr[55];
-
+// 내림차순으로 정리하고 싶을 경우 부등호는 > 를 사용한다. 등호는 일반적으로 사용하지 말자.
 bool cmp(const string& a, const string& b) {
     return a + b > b + a;
 }
@@ -18,6 +18,7 @@ int main() {
     {
         cin >> arr[i];
     }
+    
     sort(arr, arr + n, greater<>());
     vector<string> vec;
     for (int i = 0; i < n; i++)
@@ -25,6 +26,7 @@ int main() {
         vec.push_back(to_string(arr[i]));
         k--;
     }
+    // 가장 큰 값을 최대한 많이 넣는 것이 최적의 해를 보장하는 것을 알 수 있다.
     while (k--)
     {
         vec.push_back(to_string(arr[0]));
