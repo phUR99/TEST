2072ce2a81657a81c09e27bd1d2116cf2f4b7d93
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-27 20:40:34 +0900

    [Gold IV] Title: 플로이드, Time: 28 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+
+const int INF = 0x3f3f3f3f;
+int d[105][105];
+int n, m;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        fill(d[i], d[i] + n + 1, INF);
+    }
+    while (m--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        d[a][b] = min(d[a][b], c);
+    }
+    for (int i = 1; i <= n; i++) d[i][i] = 0;
+    for (int k = 1; k <= n; k++) for (int i = 1; i <= n; i++) for (int j = 1; j <= n; j++) {
+        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++) {
+            if (d[i][j] == INF) cout << "0 ";
+            else cout << d[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
