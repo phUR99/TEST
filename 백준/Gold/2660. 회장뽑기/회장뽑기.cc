1bca66af78bac7213a422fa9d95f8cc7bec849b9
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-28 20:48:30 +0900

    Update 회장뽑기.cc

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -4,7 +4,10 @@ using namespace std;
 #define ll long long
 #define X first
 #define Y second
-
+//idea : n이 작으니 가능한 n에 대해서 완전탐색을 시도한다.
+//bfs를 모든 n에 시도하면서 dist 배열을 전달한다.
+//dist 배열의 값이 -1이 아니면 방문한 배열이므로 방문 X
+//bfs가 끝날 때 최댓갓을 체크해서 ans배열에 전달해준다.
 
 vector<int> adj[55];
 int ans[55];
