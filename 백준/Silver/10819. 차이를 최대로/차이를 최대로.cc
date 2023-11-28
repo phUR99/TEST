34dade4a7d9d95e7a59cb7d6494084a5eaf41df5
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-29 00:37:29 +0900

    Update 차이를 최대로.cc

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
@@ -7,7 +7,9 @@ using namespace std;
 int n;
 int ans = 0;
 int arr[10];
-
+//idea: n이 충분히 작으므로 가능한 경우를 모두 탐색할 수 있다.
+//따라서 받은 배열을 순열을 이용해서 모두 찾는다.
+//tip: next_permutation 함수는 오름차순으로 시작해서 내림차순으로 정렬한다. 모든 경우를 탐색하고 싶다면 초기에 배열을 오름차순으로 정렬하자.
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
