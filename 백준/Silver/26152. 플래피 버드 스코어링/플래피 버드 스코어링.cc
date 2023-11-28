156e343f66fa661b24b9a653e7dbe9c0dcc53b69
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-28 15:24:40 +0900

    Update 플래피 버드 스코어링.cc

--- 백준/Silver/26152. 플래피 버드 스코어링/플래피 버드 스코어링.cc
+++ 백준/Silver/26152. 플래피 버드 스코어링/플래피 버드 스코어링.cc
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+//헤맨 이유: 벡터를 페어로 설정했는데 설정함에 따라서 이분탐색을 위해서 벡터를 정렬한 후 다시 나눌 필요가 있었다. 이 부분에서 헤맴.
+//min_element가 o(n)의 복잡도를 가진다는 것을 몰랐다. 
 using namespace std;
 #define ll long long
 #define X first
@@ -51,3 +52,39 @@ int main() {
     }
 
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+  int n;
+  cin >> n;
+  vector<int> v;
+  int tmp;
+  for (int i = 0; i < n; i++) {
+    cin >> tmp;
+    v.push_back(tmp);
+  }
+  for (int i = 0; i < n; i++) {
+    cin >> tmp;
+    v[i] -= tmp;
+  }
+
+  for (int i = 1; i < n; i++) {
+    v[i] = min(v[i-1], v[i]);
+  }
+  reverse(v.begin(), v.end());
+
+  int q;
+  cin >> q;
+  for (int i = 0; i < q; i++) {
+    cin >> tmp;
+    cout << n - (lower_bound(v.begin(), v.end(), tmp) - v.begin()) << '\n';
+  }
+}
+*/
