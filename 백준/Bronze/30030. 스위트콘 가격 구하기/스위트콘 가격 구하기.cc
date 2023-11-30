2149efd32d18c71a8011d3172ec2a8faea2e1561
phUR99 <122969226+phUR99@users.noreply.github.com>
2023-11-30 21:24:11 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,14 @@
+ #include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int b;
+    cin >> b;
+    int a = (b / 11) *10;
+    cout << a;
+}
