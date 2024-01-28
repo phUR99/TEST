a5a8a7718af564a4c9c5e4448eb90d9fbcec8a00
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-01-29 00:26:48 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,18 +1,19 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+int arr[10];
 
-int main(void) {
-	int arr[10] = {};
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	string str;
 	cin >> str;
-	for (auto s : str) {
-		if (s == '6' || s == '9') {
-			if (arr[6] < arr[9]) arr[6]++;
-			else arr[9]++;
+	for (auto e : str)
+	{
+		if (e != '6' && e != '9') arr[e - '0']++;
+		else {
+			if (arr[6] > arr[9]) arr[9]++;
+			else arr[6]++;
 		}
-		else arr[s - '0']++;
 	}
 	cout << *max_element(arr, arr + 10);
 }
