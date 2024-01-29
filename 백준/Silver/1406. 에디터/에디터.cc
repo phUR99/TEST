011a3bdf6034cd7c154d56ec33351dcd26cb3c2a
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-01-29 17:35:01 +0900

    [Silver II] Title: 에디터, Time: 72 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,52 +1,38 @@
-#include <iostream>
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <list>
+#include <bits/stdc++.h>
 using namespace std;
-//list를 이용해서 해결하는 문제
-
-int main(void) {
+list<char> L;
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	string str;
 	cin >> str;
-	list<char> l;
-	//리스트 생성
-	for (auto i : str)
-	{
-		l.push_back(i);
-	}
-	//end를 이용하면 맨 뒤의 위치의 iter.를 알아낼 수 있다.
-	auto cursor = l.end();
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
+	for (auto e : str)
 	{
-		char temp;
-		cin >> temp;
-		if (temp == 'P') {
+		L.push_back(e);
+	}
+	auto t = L.end();
+	while (n--)
+	{
+		char  c;
+		cin >> c;
+		if (c == 'L') if (t != L.begin()) t--;
+		if (c == 'D') if (t != L.end()) t++;
+		if (c == 'B') if (t != L.begin()) {
+			t--;
+			t = L.erase(t);
+		}
+		if (c == 'P') {
 			char tmp;
 			cin >> tmp;
-			//cursor의 앞에 tmp 삽입
-			l.insert(cursor, tmp);
+			L.insert(t, tmp);
+
 		}
-		else if (temp == 'L') {
-			if (cursor != l.begin()) cursor--;
-		}
-		else if (temp == 'D') {
-			if (cursor != l.end()) cursor++;
-		}
-		else {
-			if (cursor != l.begin()) {
-				cursor--;
-				//erase는 제거된 마지막 원소의 following을 가리키는 iter를 반환해줌.
-				cursor = l.erase(cursor);
-			}
-		}
+
 	}
-	for (auto i : l)
+	for (auto e : L)
 	{
-		cout << i;
+		cout << e;
 	}
 }
