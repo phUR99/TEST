f50e2cc657b9a51932773c76664d96651539d3cd
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-01-29 18:12:44 +0900

    [Silver II] Title: 키로거, Time: 196 ms, Memory: 18740 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -1,56 +1,44 @@
-#include <iostream>
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <list>
+#include <bits/stdc++.h>
 using namespace std;
-//연결 리스트를 이용하는 문제
-void keyloger(string a) {
-	list<char> l;
-	// 비어있어도 주소는 있으니까
-	auto cursor = l.begin();
-	for (auto i : a) {
-		if (l.empty()) {
-			if (isalnum(i)) {
-				//insert!!
-				l.insert(cursor, i);			
-			}
-		}
-		else {
-			if (i == '<') {
-				if (cursor != l.begin()) cursor--;
-			}
-			else if (i == '>') {
-				if (cursor != l.end()) cursor++;
-			}
-			else if (i == '-') {
-				if (cursor != l.begin()) {
-					cursor--;
-					cursor = l.erase(cursor);
-				}
-			}
-			else {
-				l.insert(cursor, i);		
-			}
-		}
-	}
-	for (auto i : l)
-	{
-		cout << i;
-	}
-	cout << '\n';
-}
 
-int main(void) {
+list<char> L;
+
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+	string str;
 	int n;
 	cin >> n;
-	string temp;
-	for (int i = 0; i < n; i++)
+	while (n--)
 	{
-		cin >> temp;
-		keyloger(temp);
+		cin >> str;
+		auto t = L.begin();
+		for (auto e : str)
+		{
+			if (e =='<')
+			{
+				if (t != L.begin()) t--;
+			}
+			else if (e == '>') {
+				if (t != L.end()) t++;
+			}
+			else if (e == '-') {
+				if (t != L.begin() && !L.empty()) {
+					t--;
+					t = L.erase(t);
+				}
+			}
+			else
+			{
+				L.insert(t, e);
+			}
+		}
+		for (auto e : L)
+		{
+			cout << e;
+		}
+		cout << '\n';
+		L.clear();
 	}
+
 }
