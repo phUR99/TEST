b2091ecaf5825b150b0d081a48439ce107516819
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-01-30 21:45:30 +0900

    [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 12496 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,40 +1,28 @@
-#include <iostream>
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <stack>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+int arr[1000001];
+int ans[1000001];
 
-int main(void) {
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	stack<pair<int,int>> s;
-	vector<pair<int, int>> ans;
-	int n;
-	cin >> n;
-	int temp;
-	int cnt;
-	for (int i = 1; i <= n; i++)
+	stack<int> s;
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
 	{
-		cnt = 0;
-		cin >> temp;
-		while (!s.empty() && s.top().first < temp)
-		{
-			ans.push_back({ s.top().second, temp });
-			s.pop();
-		}
-		s.push({ temp, i });
-
+		cin >> arr[i];
 	}
-	while (!s.empty())
+	for (int i = t-1; i >= 0; i--)
 	{
-		ans.push_back({ s.top().second, -1 });
-		s.pop();
+		while (!s.empty() && s.top() <= arr[i]) s.pop();
+		if (s.empty()) ans[i] = -1;
+		else ans[i] = s.top();
+		s.push(arr[i]);
 	}
-	sort(ans.begin(), ans.end());
-	for (auto i : ans)
+	for (int i = 0; i < t; i++)
 	{
-		cout << i.second << " ";
+		cout << ans[i] << ' ';
 	}
+	return 0;
 }
