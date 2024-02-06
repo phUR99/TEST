ceb738edf456c2e6cb600a51431e6fcb5d67d516
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-02-06 23:20:16 +0900

    Update 회전하는 큐.cc

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+//idea: 2번을 계속해서 찾으나 3번을 계속해서 찾으나 결국에 답을 찾는다면 동일한 형태가 되는 것에 주목
+//한쪽 방향으로 계속 탐색한 후에 덱의 사이즈에서 그 값을 빼준다면 반대방향으로 탐색한 결과를 도출할 수 있다.
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
