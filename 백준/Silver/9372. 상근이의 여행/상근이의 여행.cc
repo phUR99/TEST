845f7c4bfa08378ab08360522c9014901ef8b024
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-02-19 21:00:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+1. union_find 해결
+2. 최소의 간선의 개수는 노드 -1
+*/
+
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		int a, b;
+		while (m--)
+		{
+			cin >> a >> b;
+		}
+		cout << n - 1 << '\n';
+	}
+}
