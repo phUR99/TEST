9f0bae18c137577a59d78c3bb18a4d31da7db041
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-03-02 15:43:48 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -1,51 +1,53 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <math.h>
-#include <tuple>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> a;
-int num[30];
-int n, m;
-int cnt = 0;
+/*
 
-void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++)
+*/
+int num[9];
+int arr[9];
+bool isUsed[9];
+int n, m;
+// idx번째를 현재 함수를 실행시켜서 채우고, 그 이후는 자기 자신을 불러서 동일한 방식으로 해결하는 방식의 백트래킹 구현
+void func(int idx, int cur) {
+	//기저조건 1: 사용한 숫자면 종료한다.
+	//if (isUsed[cur]) return;
+	//기저조건 2: m번째 idx까지 배열을 채웠을 경우 그 값을 출력하고 종료한다.
+	//주의 : 기저조건 2에서 함수를 종료시켰을 경우 isused[cur] = false 구문을 실행하지 않고 종료시키게 되어 탐색을 하지 않는 문제가 생김.
+	arr[idx] = num[cur];
+	//isUsed[cur] = true;
+	if (idx == m) {
+		for (int i = 1; i <= m; i++)
 		{
-			cout << a[num[i]] << ' ';
+			cout << arr[i] << ' ';
 		}
 		cout << '\n';
-		return;
 	}
-	int st = 0;
-	if (k != 0) st = num[k - 1];
-	for (int i = st; i < n; i++)
+	else
 	{
-		num[k] = i;
-		func(k + 1);
+		cur = (cur == 0) ? 1 : cur;
+		for (int i = cur; i <= n; i++)
+		{
+			func(idx + 1, i);
+		}
 	}
-
-
+	//isUsed[cur] = false;
+	return;
 }
 
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
 
-	for (int i = 0; i < n; i++)
+
+int main() {	
+	ios_base::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+	/////////////////////////////////////////////////////////////////
+	
+	cin >> n >> m;	
+	for (int i = 1; i <= n; i++)
 	{
-		int temp;
-		cin >> temp;
-		a.push_back(temp);
+		cin >> num[i];
 	}
-	sort(a.begin(), a.end());
-	func(0);
+	sort(num + 1, num + n + 1);
+	func(0, 0);
+	/////////////////////////////////////////////////////////////////
+	return 0;
 }
-
