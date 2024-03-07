f735bdc9c7f816325b141ac0180f4dcd805160ca
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-03-07 22:30:58 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -30,7 +30,6 @@ int main(void) {
 	}
 
 	int len = b.size();
-	//저장된 벡터를 그대로 사용하면 시간초과가 나기 때문에 check 벡터를 만들고 순열을 돌려서 넘기는 방식으로 해결하기
 	vector<int> check((int)v.size());
 	for (int i = 0; i < m; i++) {
 		check[i] = 1;
@@ -47,7 +46,6 @@ int main(void) {
 			int cur_dist = 10000;
 			for (int j = 0; j < v.size(); j++)
 			{
-				// 값이 0이면 넘긴다.
 				if (check[j] == 0) continue;
 				cur_dist = min(cur_dist, abs(x - v[j].X) + abs(y - v[j].Y));
 			}
