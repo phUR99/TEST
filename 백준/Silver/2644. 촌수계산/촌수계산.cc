fb393e409cfe9136b7a6e5ea588f9240fca2ff9c
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-03-15 00:02:18 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -1,48 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MX = 987654321;
-/*
-#17142 : 2의 개수 중에서 m개를 뽑고 BFS를 실행한 후에 최댓값을 센다 -> 50*50 *10C5 -> 600000
+vector<int> adj[105];
+bool isUsed[105];
 
-*/
-
-
-bool visited[101];
-vector<int> adj[101];
 int n;
-int st, ed;
-int m;
 
-int DFS(int st, int cnt) {
-	if (st == ed) {
-		return cnt;
-	}
-	int ret = -1;
-	for (auto nxt : adj[st])
-	{
-		if (visited[nxt] == true) continue;
-		visited[nxt] = true;
-		ret = max(ret, DFS(nxt, cnt + 1));
-		visited[nxt] = false;
-	}
-
-	return ret;
+void findCnt(int cur, int ed, int cnt){
+    if(isUsed[cur] == true) return;
+    isUsed[cur] = true;
+    if(cur == ed) {
+        cout << cnt;
+        exit(0);
+    }    
+    for (auto nxt : adj[cur])
+    {
+       findCnt(nxt, ed, cnt+1);
+    }
+    isUsed[cur] = false;
+    return;
 }
 
-int main() {
-	ios_base::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
-	/////////////////////////////////////////////////////////////////
-	cin >> n >> st >> ed >> m;
-	while (m--)
-	{
-		int u, v;
-		cin >> u >> v;
+int main(){
+    int st, ed;
+    cin >> n >> st >> ed;
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    findCnt(st, ed, 0);
+    cout << -1;
+    
+    
 
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-	}
-	int ans = DFS(st, 0);
-	cout << ans;
-	/////////////////////////////////////////////////////////////////
-	return 0;
+
+    return 0;
 }
