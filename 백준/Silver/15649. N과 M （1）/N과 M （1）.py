5543c53c5c2e455bbe9ed4a60cc9e1a5d15f196f
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-03-14 21:28:16 +0900

    Update N과 M （1）.py

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,9 +1,11 @@
 n, m = map(int, input().split())
 arr = [0 for i in range(m)]
+#n까지의 정보가 필요하므로 range의 범위를 n+1로 설정해야함
 isUsed = [False for i in range(n+1)]
 def comb(cnt):
     
     if(cnt == m):
+        #arr -> string의 형태로 변환시킨 후에 원소 + ' '의 형태로 출력함
         print(' '.join(map(str, arr)))
         return
 
