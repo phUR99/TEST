81b666c11e589abb2e3ae58c143814358bbe688e
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-05-16 22:41:15 +0900

    240516

--- SOC/week10~11/homework2.v
+++ SOC/week10~11/homework2.v
@@ -5,7 +5,8 @@ module RisingEdgeDetector (
 );
     localparam  S0 =  2'b00,
                 S1 =  2'b01,
-                S2 =  2'b10;
+                S2 =  2'b10,
+                S3 =  2'b11;
     reg [1:0] pre_state, nxt_state;
     wire left, right;
 
@@ -20,19 +21,26 @@ module RisingEdgeDetector (
         .i_switch(PUSH_SW_RIGHT),
         .o_switch(right)
     );
-    assign LED = (pre_state == S1) ? 1'b1 : 1'b0;
+    assign LED = (pre_state == S3) ? 1'b1 : 1'b0;
 
     always @(pre_state or left or right) begin
         
         /*
+        left = 0, right = 1;
+        0
         S0 left S0, right S1
-        S1 left S0, right S2
-        S2 left S0, right S2
+        1
+        S1 left S2, right S1
+        10
+        S2 left S0, right S3
+        101
+        S3 left S2, right S1
         */
         case (pre_state)
             S0 : if (left) nxt_state = S0; else if(right) nxt_state = S1; else nxt_state = S0;
-            S1 : if (left) nxt_state = S0; else if(right) nxt_state = S2; else nxt_state = S2;
-            S2 : if (left) nxt_state = S0; else if(right) nxt_state = S2; else nxt_state = S2;
+            S1 : if (left) nxt_state = S2; else if(right) nxt_state = S1; else nxt_state = S1;
+            S2 : if (left) nxt_state = S0; else if(right) nxt_state = S3; else nxt_state = S2;
+            S3 : if (left) nxt_state = S2; else if(right) nxt_state = S1; else nxt_state = S3;
             default: nxt_state = S0;
         endcase
     end           
