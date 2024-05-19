edb804cef052b8ec139fa3d1fe82d150b05ad5ed
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-05-19 05:54:08 -0700

    [Gold IV] Title: 회전 초밥, Time: 984 ms, Memory: 13764 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, d, k, c;
+int arr[3000000 + 3000 + 50];
+int kind[3050];
+int sum = 1, ans;
+
+int main(){
+
+    cin >> N >> d >> k >> c;
+    kind[c]++;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = N; i < N + k; i++)
+    {
+        arr[i] = arr[i-N];
+    }
+    for (int i = 0; i < k; i++)
+    {
+        if (kind[arr[i]] == 0) {
+            sum++;
+        }
+        kind[arr[i]]++;
+    }
+    ans = max(ans, sum);
+
+    for (int i = k; i < N+k; i++)
+    {
+        if (kind[arr[i]] == 0) sum++;            
+        kind[arr[i]]++;
+        int end = i - k;
+        kind[arr[end]]--;
+        if(kind[arr[end]] == 0) sum--;
+        ans = max(ans, sum);
+    }
+    cout << ans;
+    
+
+    
+
+
+    return 0;
+}
