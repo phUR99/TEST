dfdc77b2227df7839da484b35608eea27b6084e6
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-06-03 20:22:53 +0900

    240603

--- ESD/week3/branch.s
+++
@@ -1,4 +0,0 @@
-       MOV     R0, #0XFF
-       TST     R0, #0B10000000
-       MOVEQ   R1, #3
-       MOVNE   R1, #2
--- SOC/week9~10/sources/10bit_ledctrl.v
+++
@@ -1,7 +0,0 @@
-module led_ctrl_10bit(
-    input [9:0] sig_sel,
-    output [9:0] sig_led
-);    
-    assign sig_led = sig_sel;
-
-endmodule
