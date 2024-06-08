c9ce15560706312d0965ded26cfb26ead5a2962c
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-06-07 23:52:30 -0700

    [Gold IV] Title: 최단경로, Time: 96 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,54 +1,63 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <tuple>
 
 using namespace std;
-#define ll long long
-#define X first
-#define Y second
+/*
+한 점에 대해서 다른 정점으로 가는 최단거리 ->다익스트라
 
-int v, e, st;
-//비용, #정점
-vector<pair<int, int>> adj[20'005];
-const int INF = 1e9 + 10;
-//최단 거리 테이블
-int d[20'005];
+*/
+const int INF = 0x7fffffff;
+int node[20005];
+
+struct Edge
+{
+	int to, cost;
+	bool operator<(const Edge &a) const {
+		return a.cost < cost;
+	}
+};
+
+vector<Edge> adj[20005];
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> v >> e >> st;
-    //INF으로 거리 테이블을 채워줌
-    fill(d, d + v + 1, INF);
-    while (e--)
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        //가중치, 정점으로 그래프를 만들어줌
-        adj[u].push_back({ w, v });
+	//빠른 입출력을 위해서 사용하기
+	ios_base::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
 
-    }
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    //시작할 점의 값을 0으로
-    d[st] = 0;
-    //거리, 시작점의 값으로 우선순위 큐를 만들어줌
-    pq.push({ d[st], st });
-    while (!pq.empty())
-    {
-        auto cur = pq.top(); pq.pop();
-        //우선순위 큐의 top의 원소의 거리와 최단거리 테이블에서 찾아온 원소의 테이블이랑 일치하지 않으면 버린다.
-        if (d[cur.Y] != cur.X) continue;
-        //기준 정점에서 갈 수 있는 원소들을 모두 체크한다.
-        for (auto nxt : adj[cur.Y])
-        {
-            // 테이블에서 찾아온 값이 원소의 가중치 + 현재 정점보다 클 경우만 값을 갱신해준다.
-            if (d[nxt.Y] <= d[cur.Y] + nxt.X) continue;
-            d[nxt.Y] = d[cur.Y] + nxt.X;
-            // 다시 그 값을 우선순위 큐에 넣어준다.
-            pq.push({ d[nxt.Y], nxt.Y });
-        }
-    }
-    for (int i = 1; i <= v; i++)
-    {
-        if (d[i] == INF) cout << "INF\n";
-        else cout << d[i] << "\n";
-    }
+	//init
+	int v, e, st;
+	cin >> v >> e >> st;
+
+	fill(node + 1, node + v + 1, INF);
+
+	while (e--)
+	{
+		int from, to, cost;
+		cin >> from >> to >> cost;
+		adj[from].push_back({ to, cost });
+	}
+	priority_queue<Edge> pq;
+	pq.push({ st, 0 });
+	node[st] = 0;
+
+	while (!pq.empty())
+	{
+		auto cur = pq.top(); pq.pop();
+		if (node[cur.to] < cur.cost) continue;
+		for (auto nxt : adj[cur.to])
+		{
+			if (node[nxt.to] > cur.cost + nxt.cost) {
+				node[nxt.to] = cur.cost + nxt.cost;
+				pq.push({ nxt.to, node[nxt.to] });
+			}
+		}
+	}
+	for (int i = 1; i <= v; i++)
+	{
+		if (node[i] == INF) cout << "INF" << '\n';
+		else    cout << node[i] << '\n';
+	}
+	return 0;
 }
