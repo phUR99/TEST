82db43deb363287e2d23911a607a62d4c2d8ca9d
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-06-27 02:33:05 -0700

    [Gold V] Title: 합분해, Time: 24 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -1,46 +1,32 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <math.h>
-#include <tuple>
-#include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-ll d[205][205];
-int di = 1'000'000'000;
-//d[1][k]를 엄청 헤맸다.
-//피라미드 식으로 구현 가능
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n, k;
-	cin >> n >> k;
-	
-	for (int i = 1; i <= n ; i++)
-	{
-		d[i][1] = 1;
-	}
-	for (int i = 1; i <= k; i++)
-	{
-		d[1][i] = i;
-	}
-	fill(d[0] + 1, d[0] + k + 1, 1);
-	for (int i = 2; i <=k ; i++)
-	{
-		for (int j = 2; j <= n; j++)
-		{
-			
-			for (int l = 0; l <= j ; l++)
-			{
-				d[j][i] += d[l][i - 1] % di;
-				d[j][i] %= di;
-			}
-		}
-	}
-	cout << d[n][k] %di;
+int N, K;
+int cache[205][205];
 
+
+int calculateNumber(int n, int k){
+    if(n == 0 && k == 0) return 1;
+    else if (n < 0) return 0;
+    else if (k == 0) return 0;
+
+    int &ret = cache[n][k];
+    if(ret != -1) return ret;
+    ret = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        ret += calculateNumber(n-i, k-1);
+        ret %= 1'000'000'000;
+    }
+    return ret;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> K;
+    memset(cache, -1, sizeof(cache));
+
+    cout << calculateNumber(N, K);
+
+    return 0;
 }
