5247ce82faf5335a9892d65c1071d24b37c0b39d
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-07-11 08:02:53 -0700

    [Gold III] Title: 순회강연, Time: 28 ms, Memory: 2260 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define prize first
+#define deadline second
+int arr[10005];
+
+int N;
+
+int main(){
+    fastio;
+    cin >> N;
+    vector<pair<int, int>> v(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i].prize >> v[i].deadline;        
+    }
+    sort(v.begin(), v.end(), greater<>());
+    int ans = 0;
+    for (int i = 0; i < N; i++)
+    {        
+        for (int j = v[i].deadline; j > 0; j--)
+        {
+            if(!arr[j]){
+                arr[j] = v[i].prize;
+                ans += arr[j];
+                break;
+            }
+        }        
+    }
+    cout << ans;
+    
+
+    return 0;
+}
