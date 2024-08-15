b29edadb385a00b244c7dba467291c4f795ee716
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-08-14 21:00:59 -0700

    [Platinum V] Title: 다이제스타, Time: 8 ms, Memory: 6604 KB, Score: 29 point -BaekjoonHub

--- 백준/Platinum/25430. 다이제스타/다이제스타.cc
+++ 백준/Platinum/25430. 다이제스타/다이제스타.cc
@@ -3,7 +3,7 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define ll long long
 const ll inf = 0x3fffffffffffffff;
-ll dist[50050];
+map<int, ll> dist[50050];
 int n, m;
 vector<pair<int, int>> adj[50050];
 struct node
@@ -17,8 +17,7 @@ struct node
 
 int main(){
     fastio;
-    cin >> n >> m;
-    fill(dist, dist+50050, inf);
+    cin >> n >> m;    
     
     int u, v, c;
     for (int i = 0; i < m; i++)
@@ -26,28 +25,34 @@ int main(){
         cin >>u >> v >> c;
         adj[u].push_back({c, v});
         adj[v].push_back({c, u});
+        dist[u][c] = inf;
+        dist[v][c] = inf;
     }
     int s, e;
     cin >> s >> e;
-    dist[s] = 0;
+    dist[s][0] = 0;
 
     priority_queue<node> pq;
     pq.push({0, 0, s});
+    ll answer = inf;
     while (!pq.empty())
     {
         node cur = pq.top(); pq.pop();
-        if(cur.dist > dist[cur.edge]) continue;
+        if(cur.dist > dist[cur.edge][cur.pre]) continue;
         for (auto nxt : adj[cur.edge])
         {
-            if(cur.pre < nxt.first && dist[nxt.second] > nxt.first + cur.dist){
-                dist[nxt.second] = nxt.first + cur.dist;
+            if(cur.pre < nxt.first && dist[nxt.second][nxt.first] > cur.dist + nxt.first){
+                dist[nxt.second][nxt.first] = nxt.first + cur.dist;
                 int pre = nxt.first;
-                pq.push({dist[nxt.second], pre, nxt.second});
+                pq.push({nxt.first + cur.dist, pre, nxt.second});
+                if(nxt.second == e){
+                    answer  = min(answer, nxt.first + cur.dist);                    
+                }
             }
         }        
     }
-    if(dist[e] == inf) cout << "DIGESTA";
-    else cout << dist[e];
-
+    if(answer == inf) cout << "DIGESTA";
+    else cout << answer;
+    
     return 0;
 }
