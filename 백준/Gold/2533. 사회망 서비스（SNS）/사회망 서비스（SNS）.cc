b47aeecda2a040681656f25c983b7609aa2f2344
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-11-19 20:53:56 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 796 ms, Memory: 62352 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -1,45 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-int N;
-vector<int> adj[1'000'005];
-int cache[1'000'005][2];
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long
+int n;
+vector<vector<int>> adj;
+bool visited[1000005];
+int answer = 0;
+const int need = 0;
+const int unneed = 1;
 
-int solution(int prev, int cur, int curState){
-\
-    int &ret = cache[cur][curState];
-    if(ret != -1) return ret;
-    ret = curState;
-
-    for (auto child : adj[cur])
+int solve(int here)
+{
+    visited[here] = true;
+    vector<int> status(2, 0);
+    for (auto there : adj[here])
     {
-        if(prev == child) continue;
-
-        int tmp = 987654321;
-        if(curState){
-            tmp = min(solution(cur, child, 0), solution(cur, child, 1));
-        }
-        else{
-            tmp = solution(cur, child, 1);
-        }
-        ret += tmp;
-    }    
-    return ret;
+        if (visited[there])
+            continue;
+        ++status[solve(there)];
+    }
+    if (status[need])
+    {
+        answer++;
+        return unneed;
+    }
+    return need;
 }
-
-int main(){
+int main()
+{
     fastio;
-
-    cin >> N;
-    for (int i = 1; i < N; i++)
+    cin >> n;
+    adj.resize(n + 1);
+    for (int i = 0; i < n - 1; i++)
     {
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    memset(cache, -1, sizeof(cache));        
-    int ans = min(solution(-1, 1, 1), solution(-1, 1, 0));
-    cout << ans;
+    solve(1);
+    cout << answer << '\n';
+
     return 0;
 }
