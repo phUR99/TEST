611faff14d5d2ea9ca81ff7eebe8d08c852b6ae5
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-11-29 13:45:36 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 92 ms, Memory: 9552 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long
+int n, m, r, counter = 0;
+int discovered[100005];
+vector<vector<int>> adj;
+void bfs(int here)
+{
+    queue<int> q;
+    q.push(here);
+    discovered[here] = ++counter;
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        for (int nxt : adj[cur])
+        {
+            if (discovered[nxt] == -1)
+            {
+                q.push(nxt);
+                discovered[nxt] = ++counter;
+            }
+        }
+    }
+}
+
+int main()
+{
+    fastio;
+    memset(discovered, -1, sizeof(discovered));
+    cin >> n >> m >> r;
+    adj.resize(n + 1);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        sort(adj[i].begin(), adj[i].end());
+    }
+    bfs(r);
+    for (int i = 1; i <= n; i++)
+    {
+        if (discovered[i] == -1)
+            discovered[i] = 0;
+        cout << discovered[i] << '\n';
+    }
+
+    return 0;
+}
