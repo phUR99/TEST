cf838aaba50fa2883333bd36c1879977aeb249d1
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-12-13 01:49:18 +0900

    [Gold V] Title: 선 긋기, Time: 348 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-// 그리디? : st와 ed를 비교하면서 st < ed일 경우 계속해서 최선의 경우를 찾을 수 있음.
-// iterator 체크하기 i++를 break문 앞에 둘 경우에는 n+1 을 체크하는 것이 아닌 n+2를 체크하기 때문에 오류 가능
-// ed의 길이가 원소가 작을 때 큰 경우 (1, 4) (2, 3)이 있을 수 있기 때문에 ed의 길이는 max를 사용해서 갱신해준다.
+
 using namespace std;
 #define ll long long
 
