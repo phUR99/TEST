4f22b6424564eacac42c2722f0eece35d73e051c
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-12-16 13:26:28 +0900

    [Gold III] Title: 아침 산책, Time: 0 ms, Memory: 2264 KB, Score: 60 point -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long
+
+vector<vector<int>> adj;
+int outside[20005];
+bool visited[20005];
+int n;
+int dfs(int here)
+{
+    visited[here] = 1;
+    int ret = 0;
+    for (int there : adj[here])
+    {
+        if (outside[there])
+            ret++;
+        else if (!visited[there])
+            ret += dfs(there);
+    }
+    return ret;
+}
+int main()
+{
+    fastio;
+    cin >> n;
+    adj.resize(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        char c;
+        cin >> c;
+        outside[i] = c - '0';
+    }
+    int ret = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+        if (outside[a] && outside[b])
+            ret += 2;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (!visited[i] && !outside[i])
+        {
+            int tmp = dfs(i);
+            ret += tmp * (tmp - 1);
+        }
+    }
+    cout << ret;
+    return 0;
+}
