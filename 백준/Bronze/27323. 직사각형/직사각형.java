2c036996fa88124c822a47629896e16c01d68e3a
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-12-18 01:07:15 +0900

    [Bronze V] Title: 직사각형, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+        System.out.printf("%d", A * B);
+    }
+}
