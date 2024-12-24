5fb1cca04feeba82a0d7662e483acc2a780963f9
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-12-25 00:38:28 +0900

    [Gold II] Title: 피보나치 수 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long
+const vector<vector<ll>> matrix = {{1, 1},
+                                   {1, 0}};
+const ll MOD = 1e6;
+
+vector<vector<ll>> multiply(const vector<vector<ll>> &a, const vector<vector<ll>> &b)
+{
+    auto ret = vector<vector<ll>>(2, vector<ll>(2));
+    ret[0][0] = ((a[0][0] % MOD) * (b[0][0] % MOD) + (a[0][1] % MOD) * (b[1][0] % MOD)) % MOD;
+    ret[0][1] = ((a[0][0] % MOD) * (b[0][1] % MOD) + (a[0][1] % MOD) * (b[1][1] % MOD)) % MOD;
+    ret[1][0] = ((a[1][0] % MOD) * (b[0][0] % MOD) + (a[1][1] % MOD) * (b[1][0] % MOD)) % MOD;
+    ret[1][1] = ((a[1][0] % MOD) * (b[0][1] % MOD) + (a[1][1] % MOD) * (b[1][1] % MOD)) % MOD;
+    return ret;
+}
+
+vector<vector<ll>> solve(ll n)
+{
+    if (n == 0)
+    {
+        vector<vector<ll>> ret = {{1, 0},
+                                  {0, 1}};
+        return ret;
+    }
+    if (n & 1)
+        return multiply(matrix, solve(n - 1));
+    auto half = solve(n / 2);
+    return multiply(half, half);
+}
+
+int main()
+{
+    fastio;
+    // n이 ll 범위로 들어온다
+    ll n;
+    cin >> n;
+    auto arr = solve(n);
+    cout << arr[1][0] % MOD;
+    return 0;
+}
