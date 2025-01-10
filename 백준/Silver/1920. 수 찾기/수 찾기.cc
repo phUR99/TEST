05b60e4e51d915f6e215de268545e5159bc07bb1
phUR99 <122969226+phUR99@users.noreply.github.com>
2025-01-10 17:19:50 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,31 +1,61 @@
-//이분탐색을 써야하는 이유
-//최악의 경우 10^5번의 탐색을 진행해야 하는데, 순차 탐색일 경우에는 o(N^2)이므로 10억번의 연산이 넘게 필요할 수도 있다.
-//무조건 시간 초과가 뜰 것이므로 algortihm 헤더의 binary_search 를 이용해서 i가 있는 지를 탐색한다.
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
+bool binary(vector<int> &nums, int target);
+int main() 
+{
+    ios::sync_with_stdio(false); // 입출력 성능 향상
+    cin.tie(nullptr);           // 입출력 버퍼 비우기 비활성화
+    cout.tie(NULL);
 
-int main() {
-	int n, m;
-	vector<int>	A, B;
-	cin >> n;
-	int temp;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> temp;
-		A.push_back(temp);
-	}
-	cin >> m;
-	for (int i = 0; i < m; i++)
-	{
-		cin >> temp;
-		B.push_back(temp);
-	}
-	sort(A.begin(), A.end());
-	for (auto i : B)
-	{
-		cout << binary_search(A.begin(), A.end(), i) << "\n";
-	}
-	
+    int count,n_Count;
+    cin >> count;
+    vector<int> f_Nums;
+
+    for (int i = 0; i < count; i++)
+    {
+        int n;
+        cin >> n;
+        f_Nums.push_back(n);
+    }
+
+    sort(f_Nums.begin(), f_Nums.end());
+
+    cin >> n_Count;
+    for (int i = 0; i < n_Count; i++)
+    {
+        int n;
+        cin >> n;
+        
+        if (binary(f_Nums, n)) {
+            cout << 1 << '\n';
+        }
+        else
+            cout << 0 << '\n';
+    }
+
+    
+
+    
+}
+
+bool binary(vector<int> &nums, int target)
+{
+    int low = 0;
+    int high = nums.size() - 1;
+    
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (target == nums[mid]) return true;
+
+        if (target > nums[mid]) {
+            low = mid + 1;
+        }
+        
+        if (target < nums[mid])
+            high = mid - 1;
+
+    }
+
+    return false;
 }
