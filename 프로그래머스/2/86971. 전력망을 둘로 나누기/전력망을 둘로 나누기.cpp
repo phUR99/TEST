0945be19d6dabde4833287f01c60ca3c9e1f1ccf
phUR99 <skerrigen12@gmail.com>
2025-02-21 23:50:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.06 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -2,27 +2,31 @@
 
 using namespace std;
 vector<vector<int>> adj;
+vector<int> nums;
+
 int dfs(int here, int parent){
-    int ret = 0;
+    int &ret = nums[here] = 1;
     for(int there : adj[here]){
         if(there == parent) continue;
-        ret += dfs(there, here) +1;
+        ret += dfs(there, here);
     }
     return ret;
 }
 
 int solution(int n, vector<vector<int>> wires) {
     int answer = 987654321;
-    adj.resize(n+1);    
+    adj.resize(n+1);  
+    nums.resize(n+1, 0);
     for(auto wire : wires){
         int u = wire[0];
         int v = wire[1];
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+    dfs(1, -1);
     for(int i =1; i <=n; i++){
         for(int j : adj[i]){
-            int diff = abs(dfs(i, j)- dfs(j, i));
+            int diff = abs(nums[i] * 2 - n);
             answer = min(diff , answer);
         }
     }
