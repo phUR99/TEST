b2516fee2b0ba2801fac7aa01995bc812e4ae8c8
phUR99 <skerrigen12@gmail.com>
2025-04-02 20:31:47 +0900

    [Gold III] Title: 우주 탐사선, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
@@ -6,44 +6,10 @@ using namespace std;
 #define ll long long
 int n, k;
 
-vector<vector<pair<int, int>>> adj;
-pair<int, int> dijkstra(int s, int e){
-    int dist[15];
-    int trace[15];
-    memset(dist, 0x3f, sizeof(dist));
-    memset(trace, -1, sizeof(trace));
-    dist[s] = 0;
-    priority_queue<pair<int, int>> pq;
-    pq.push({0, s});
-    while (!pq.empty())
-    {
-        auto cur = pq.top(); pq.pop();
-        int cost = -cur.first;
-        int here = cur.second;
-        if(cost > dist[here]) continue;
-        for (auto nxt : adj[here])
-        {
-            int weight = nxt.first;
-            int there = nxt.second;
-            if(dist[there] > cost + weight){
-                dist[there] = cost + weight;
-                pq.push({-dist[there], there});
-                trace[there] = here;
-            }
-        }
-        
-    }
-    int retDist = dist[e];
-    int retState = 0;
-    for (int i = e; i != -1; i = trace[i])
-    {
-        retState |= (1<<i);
-    }
-    return {retDist, retState};
-    
-}
 int full_bit;
 int cache[10][1<<10];
+int dist[10][10];
+
 int solve(int st, int state){
     if(state == full_bit) return 0;
     int &ret = cache[st][state];
@@ -53,10 +19,8 @@ int solve(int st, int state){
     for (int i = 0; i < n; i++)
     {
         if(state & (1<< i)) continue;
-        auto result = dijkstra(st, i);
-        int nxtState = state | result.second;
-        int cost = result.first;
-        ret = min(ret, cost + solve(i, nxtState));
+        int nxtState = state | (1<<i);
+        ret = min(ret, dist[st][i] + solve(i, nxtState));
     }
     return ret;
 }
@@ -65,20 +29,31 @@ int main()
 {
     fastio;
     cin >> n >> k;
-    adj.resize(n);
+
     full_bit = (1<<n)-1;
     memset(cache, -1, sizeof(cache));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            int d;
-            cin >> d;
-            if(i==j) continue;
-            adj[i].push_back({d, j});
+            cin >> dist[i][j];
         }
         
     }
+    for (int l = 0; l < n; l++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                dist[i][j] = min(dist[i][j], dist[i][l] + dist[l][j]);
+            }
+            
+        }
+        
+    }
+    
+    
 
     int ret = solve(k, (1 << k));
     cout << ret;
