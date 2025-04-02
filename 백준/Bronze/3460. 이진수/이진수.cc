0540d526be51f1a92d9b9079fc5585ff4c1b11ac
phUR99 <skerrigen12@gmail.com>
2025-04-02 09:42:47 +0900

    [Bronze III] Title: 이진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.cc
+++ 백준/Bronze/3460. 이진수/이진수.cc
@@ -1,28 +1,21 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <queue>
+#include<iostream>
 using namespace std;
-char arr[5] = { 'a', 'e', 'i', 'o', 'u' };
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	while (n--)
-	{
-		int temp;
-		cin >> temp;
-		int ans = 0;
-		while (temp > 0)
-		{
-			if (temp & 1) cout << ans << ' ';
-			temp = temp >> 1;
-			ans++;
-		}
-	}
+void solve(){
+    int n;
+    cin >> n;
+    for(int i =0; i < 32; i++){
+        if(n &(1<<i)) cout << i << ' ';
+    }
+    cout << '\n';
+}
 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
 }
