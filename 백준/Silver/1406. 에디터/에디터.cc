743df0cc23ef702456003ccf20442882ac60069f
phUR99 <skerrigen12@gmail.com>
2025-04-22 20:09:33 +0900

    [Silver II] Title: 에디터, Time: 72 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -20,7 +20,6 @@ int main() {
 		if (c == 'L') if (t != L.begin()) t--;
 		if (c == 'D') if (t != L.end()) t++;
 		if (c == 'B') if (t != L.begin()) {
-			//커서를 앞으로 땡기고 그 위치에 있는 원소를 삭제한 뒤에 가리키는 값까지 반환이 필요
 			t--;
 			t = L.erase(t);
 		}
