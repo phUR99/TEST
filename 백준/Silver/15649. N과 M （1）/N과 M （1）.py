472680e85bb2515c23eac5cacb45299032e42efe
phUR99 <skerrigen12@gmail.com>
2025-04-23 10:30:26 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,5 @@
+from itertools import combinations, permutations
 n, m = map(int, input().split())
-arr = [0 for i in range(m)]
-#n까지의 정보가 필요하므로 range의 범위를 n+1로 설정해야함
-isUsed = [False for i in range(n+1)]
-def comb(cnt):
-    
-    if(cnt == m):
-        #arr -> string의 형태로 변환시킨 후에 원소 + ' '의 형태로 출력함
-        print(' '.join(map(str, arr)))
-        return
-
-    for i in range(1, n+1):
-        if(isUsed[i] == True): continue
-        arr[cnt] = i
-        isUsed[i] = True
-        comb(cnt+1)
-        isUsed[i] = False
-
-comb(0)
+ret = permutations([i + 1 for i in range(n)], m)
+for r in ret:
+    print(*r, sep= ' ')
