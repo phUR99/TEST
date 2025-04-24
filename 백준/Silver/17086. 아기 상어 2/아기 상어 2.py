cdb6d4c9fe5e328a2c5df48df4ba580b16e734ba
phUR99 <skerrigen12@gmail.com>
2025-04-24 20:04:17 +0900

    [Silver II] Title: 아기 상어 2, Time: 60 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -8,13 +8,18 @@ for i in range(n):
     row = list(map(int, sys.stdin.readline().strip().split()))
     board.append(row)
     
-def bfs(x, y):
+def bfs():
     dx = [-1, 1, 0, 0, 1, 1, -1, -1]
     dy = [0, 0, -1, 1, 1, -1, -1, 1]
     dist = [[-1 for _ in range(m)] for _ in range(n)]
-    dist[x][y] = 0
+    
     q = deque()
-    q.append((x, y))
+    
+    for i in range(n):
+        for j in range(m):
+            if board[i][j]:
+                q.append((i, j))
+                dist[i][j] = 0
     
     def oob(x, y):
         return x < 0 or x >= n or y < 0 or y >= m
@@ -28,19 +33,8 @@ def bfs(x, y):
             if dist[nx][ny] != -1: continue
             dist[nx][ny] = dist[r][c] + 1
             q.append((nx, ny))
-    ret = 987654321
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 1:
-                if (x, y) == (i, j):
-                    continue
-                ret = min(ret, dist[i][j])
-    return ret
+    return dist
     
 
-ret = 0
-for i in range(n):
-    for j in range(m):
-        if not board[i][j]:
-            ret = max(ret, bfs(i, j))
-print(ret)
+ret = bfs()
+print(max(map(max, ret)))
