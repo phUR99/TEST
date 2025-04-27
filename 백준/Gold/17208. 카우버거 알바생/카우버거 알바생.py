e3582af9235d4db9e2e0ebf5cff7f1bcd0e1c4b4
phUR99 <skerrigen12@gmail.com>
2025-04-27 14:36:52 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 1672 ms, Memory: 190020 KB -BaekjoonHub

--- 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.py
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.py
@@ -1,22 +1,25 @@
 import sys
-from functools import cache
+
 
 n, m, k = map(int, sys.stdin.readline().strip().split())
 arr = []
 for _ in range(n):
     x, y = map(int, sys.stdin.readline().strip().split())
     arr.append([x, y])
-    
-@cache
+cache = [[[-1 for _ in range(k+1)] for _ in range(m+1)] for _ in range(n)]
+
 def solve(idx: int, remain_cheese: int, remain_potato: int):
     if idx == n:
         return 0
-    ret: int = 0
-    ret = solve(idx +1, remain_cheese, remain_potato)
+    if cache[idx][remain_cheese][remain_potato] != -1:
+        return cache[idx][remain_cheese][remain_potato]
+    
+    ret = solve(idx + 1, remain_cheese, remain_potato)
     nxt_cheese = remain_cheese - arr[idx][0]
     nxt_potato = remain_potato - arr[idx][1]
     if remain_cheese - arr[idx][0] >= 0 and remain_potato - arr[idx][1] >= 0:
         ret = max(ret, 1 + solve(idx+1, nxt_cheese, nxt_potato))
+    cache[idx][remain_cheese][remain_potato] = ret
     return ret
 
 ret = solve(0, m, k)
