d4c7a4685135001b8214a11938a52c5bcefa35be
phUR99 <skerrigen12@gmail.com>
2025-05-06 21:26:36 +0900

    [Gold V] Title: 창영이와 커피, Time: 360 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
@@ -0,0 +1,17 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().strip().split())
+
+arr = list(map(int, sys.stdin.readline().strip().split()))
+if sum(arr) < k:
+    print(-1)
+    exit(0)
+
+cache = [int(1e9) for _ in range(100000 + 1)]
+cache[0] = 0
+for i in range(n):
+    cache[arr[i]] = min(1, cache[arr[i]])
+    for j in range(k + 1, arr[i], -1):
+        cache[j] = min(cache[j], cache[j - arr[i]] + 1)
+
+print(-1 if cache[k] == int(1e9) else cache[k])
