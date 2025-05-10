59f18aed204f4964a01975361755372a2597e062
phUR99 <skerrigen12@gmail.com>
2025-05-10 17:00:16 +0900

    [Gold I] Title: 암호, Time: 108 ms, Memory: 71928 KB -BaekjoonHub

--- 백준/Gold/10160. 암호/암호.cc
+++ 백준/Gold/10160. 암호/암호.cc
@@ -1,45 +1,21 @@
-#include <bits/stdc++.h>
+    #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 int n, k;
 ll mod = 1e9 + 9;
-ll cache[1000005][5][2];
+ll cache[1000005];
 
-ll solve(int idx, int state, int third){
+ll solve(int idx){
     if(idx == n) return 1;
-    ll &ret = cache[idx][state][third];
+    ll &ret = cache[idx];
     if(ret != -1) return ret;
     ret = 0;
-    switch (state)
-    {
-    case 0:
-        ret += solve(idx+ 1, 0, 0) * (k - 1);
-        ret += solve(idx+ 1, 1, 0);
-        break;
-    case 1:
-        ret += solve(idx + 1, 0, 0) * (k - 2);
-        ret += solve(idx + 1, 1, 0);
-        ret += solve(idx + 1, 2, 0);   
-        break;
-    case 2:
-        ret += solve(idx + 1, 0, 0) * (k - 2);
-        ret += solve(idx + 1, 3, 0);
-        ret += solve(idx + 1, 3, 1);
-        break;
-    case 3:
-        ret += solve(idx + 1, 0, third) * (k - 2);
-        ret += solve(idx + 1, 1, third);
-        ret += solve(idx + 1, 4, third);
-        break;
-    case 4:
-        if (third)
-            ret += solve(idx +1, 3, third);
-        else    
-            ret += solve(idx +1, 1, 0);
-        ret += solve(idx + 1, 0, 0) * (k - 2);
-        break;
-    }
-    ret %= mod;
+    ret = (ret + solve(idx + 1) % mod * k) % mod;
+    if (idx + 5 <= n)
+        ret = (ret - solve(idx + 5) % mod * 2) % mod;
+    if (idx + 7 <= n)
+        ret = (ret + solve(idx + 7)) % mod;
+    ret = (ret + mod) % mod;
     return ret;
 }
 
@@ -48,7 +24,7 @@ int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     memset(cache, -1, sizeof(cache));
     cin >> n >> k;
-    ll ret = solve(0, 0, 0);
+    ll ret = solve(0);
     cout << ret;
     return 0;
 }
