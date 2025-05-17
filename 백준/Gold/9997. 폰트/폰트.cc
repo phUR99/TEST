c138ecf608cc0e9e1274c588af0ab90d9d424f9b
phUR99 <skerrigen12@gmail.com>
2025-05-17 11:06:31 +0900

    [Gold III] Title: 폰트, Time: 72 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9997. 폰트/폰트.cc
+++ 백준/Gold/9997. 폰트/폰트.cc
@@ -1,25 +1,33 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-int N,ans,a[25];
-void sol(int n,int b){
-    if(n==N) {
-        if(b==(1<<26)-1) ans++;
-        return;
-    }
-    sol(n+1,b);
-    sol(n+1,b|a[n]);
+int n;
+int arr[40];
+
+
+int solve(int idx, int state){
+    if(idx == n) return state == (1 << 26) - 1;
+    int ret = 0;
+    ret += solve(idx + 1, state);
+    ret += solve(idx + 1, state | arr[idx]);
+    return ret;
+
 }
+
 int main(){
-    cin >> N;
-    for(int i=0;i<N;i++) {
-        string s;
-        cin >> s;
-        int b=0;
-        for(auto c : s) b |= (1<<(c-'a'));
-        a[i]=b;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        int cur = 0;
+        for (auto &s : str)
+        {
+            cur |= (1<<(s - 'a'));
+        }   
+        arr[i] = cur;
     }
-    sol(0,0);
-    cout << ans;
+    int ret = solve(0, 0);
+    cout << ret;   
+
     return 0;
 }
